442e5507497611bbf28c8b0d2493e8a6d50d31bd
Ali Ajmal <aliajmal173@gmail.com>
2026-08-04 21:00:34 +0500

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.tucldqbj2vk"
+    "revision": "0.jsqi1ntdflo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
